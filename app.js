fd6d38853d3c9bc05a0b6f72d0a5fcc671ecf42c
Gaurav Hariyani <gauravhariyani12.gh@gmail.com>
2024-03-06 20:19:01 -0500

    updated

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+//all required packages
+//• Add the following declaration at the top of .js files
+/******************************************************************************
+ * ITE5315 – Assignment 2
+ * I declare that this assignment is my own work in accordance with Humber Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: Gaurav Hariyani Student ID: N01579426 Date: 03/05/2023
+ *
+ *
+ ******************************************************************************/
 var express = require("express");
 const fs = require("fs");
 var path = require("path");
 var app = express();
 const exphbs = require("express-handlebars");
-const port = process.env.port || 3000;
-app.use(express.static(path.join(__dirname, "public")));
+const port = process.env.port || 3000; //port specified
+app.use(express.static(path.join(__dirname, "public"))); //default public path
+//template engine with helpers
 app.engine(
   ".hbs",
   exphbs.engine({
@@ -19,16 +32,19 @@ app.engine(
     },
   })
 );
+//setting template engine
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//calling index view
 app.get("/", function (req, res) {
   res.render("index", { title: "Express" });
 });
+//normal users route
 app.get("/users", function (req, res) {
   res.send("respond with a resource");
 });
-
+//calling data view to show title
 app.get("/data", function (req, res) {
   let myData = path.join(__dirname, "public", "datasetA.json");
 
@@ -46,6 +62,7 @@ app.get("/data", function (req, res) {
     }
   });
 });
+//displaying all data using allData view
 app.get("/allData", function (req, res) {
   let myData = path.join(__dirname, "public", "datasetA.json");
 
@@ -64,6 +81,7 @@ app.get("/allData", function (req, res) {
     }
   });
 });
+//displaying isbn data using dataIndex view
 app.get("/data/isbn/:index", (req, res) => {
   let myData = path.join(__dirname, "public", "datasetA.json");
 
@@ -92,9 +110,11 @@ app.get("/data/isbn/:index", (req, res) => {
     }
   });
 });
+//displaying search result form isbn data using searchIsbn view
 app.get("/data/search/isbn/", (req, res) => {
   res.render("searchISBN");
 });
+//displaying search result isbn data using searchIsbnResult view
 app.post("/data/search/isbn/", (req, res) => {
   let myData = path.join(__dirname, "public", "datasetA.json");
 
@@ -114,28 +134,16 @@ app.post("/data/search/isbn/", (req, res) => {
             title: "Search Result for " + isbn,
             content: row,
           });
-          // res.write("\nTitle: " + row.title);
-          // res.write("\nAuthor: " + row.author);
-          // res.write("\nPrice: " + row.price);
-          // res.write("\npages: " + row.avg_reviews);
-          // res.write("\navg_reviews: " + row.avg_reviews);
-          // res.write("\nn_reviews: " + row.n_reviews);
-          // res.write("\nstar: " + row.star);
-          // res.write("\ndimensions: " + row.dimensions);
-          // res.write("\nweight: " + row.weight);
-          // res.write("\nlanguage: " + row.language);
-          // res.write("\npublisher: " + row.publisher);
-          // res.write("\nISBN_13: " + row.ISBN_13);
-          // res.write("\ncomplete_link: " + row.complete_link + "\n");
         }
       });
-      // res.end();
     }
   });
 });
+//displaying search result title data form using searchTitle view
 app.get("/data/search/title/", (req, res) => {
   res.render("searchTitle");
 });
+//displaying search result title data using searchTitleResult view
 app.post("/data/search/title/", (req, res) => {
   let myData = path.join(__dirname, "public", "datasetA.json");
   fs.readFile(myData, "utf8", (err, data) => {
@@ -151,19 +159,6 @@ app.post("/data/search/title/", (req, res) => {
         let book_title = row.title;
         if (book_title.includes(title)) {
           contents.push(row);
-          // res.write("\nTitle: " + row.title);
-          // res.write("\nAuthor: " + row.author);
-          // res.write("\nPrice: " + row.price);
-          // res.write("\npages: " + row.avg_reviews);
-          // res.write("\navg_reviews: " + row.avg_reviews);
-          // res.write("\nn_reviews: " + row.n_reviews);
-          // res.write("\nstar: " + row.star);
-          // res.write("\ndimensions: " + row.dimensions);
-          // res.write("\nweight: " + row.weight);
-          // res.write("\nlanguage: " + row.language);
-          // res.write("\npublisher: " + row.publisher);
-          // res.write("\nISBN_13: " + row.ISBN_13);
-          // res.write("\ncomplete_link: " + row.complete_link + "\n");
         }
       });
       res.render("searchTitleResult", {
@@ -171,10 +166,11 @@ app.post("/data/search/title/", (req, res) => {
         content: contents,
       });
       console.log(contents);
-      // res.end();
     }
   });
 });
+//displaying errors on wrong path/route
+
 app.get("*", function (req, res) {
   res.render("error", { title: "Error", message: "Wrong Route" });
 });
